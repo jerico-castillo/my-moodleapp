moodlehq/moodleapp

commit b72e247f81f2bda9bafd689af12ec493c42c1c43

MOBILE-3930 course: Store and display modules viewed and last

src/core/features/course/services/database/course.ts
@@ -18,9 +18,10 @@ import { CoreSiteSchema } from '@services/sites';
  * Database variables for CoreCourse service.
  */
 export const COURSE_STATUS_TABLE = 'course_status';
+export const COURSE_VIEWED_MODULES_TABLE = 'course_viewed_modules';
 export const SITE_SCHEMA: CoreSiteSchema = {
     name: 'CoreCourseProvider',
-    version: 1,
+    version: 2,
     tables: [
         {
             name: COURSE_STATUS_TABLE,
@@ -52,6 +53,29 @@ export const SITE_SCHEMA: CoreSiteSchema = {
                     type: 'INTEGER',
                 },
             ],
+        },
+        {
+            name: COURSE_VIEWED_MODULES_TABLE,
+            columns: [
+                {
+                    name: 'courseId',
+                    type: 'INTEGER',
+                },
+                {
+                    name: 'cmId',
+                    type: 'INTEGER',
+                },
+                {
+                    name: 'timeaccess',
+                    type: 'INTEGER',
+                    notNull: true,
+                },
+                {
+                    name: 'sectionId',
+                    type: 'INTEGER',
+                },
+            ],
+            primaryKeys: ['courseId', 'cmId'],
         },
     ],
 };
@@ -85,7 +109,7 @@ export const OFFLINE_SITE_SCHEMA: CoreSiteSchema = {
                     type: 'TEXT',
                 },
                 {
-                    name: 'timecompleted',
+                    name: 'timeaccess',
                     type: 'INTEGER',
                 },
             ],
@@ -100,6 +124,13 @@ export type CoreCourseStatusDBRecord = {
     updated: number;
     downloadTime: number;
     previousDownloadTime: number;
+};
+
+export type CoreCourseViewedModulesDBRecord = {
+    courseId: number;
+    cmId: number;
+    timeaccess: number;
+    sectionId?: number;
 };
 
 export type CoreCourseManualCompletionDBRecord = {

src/core/features/course/services/course.ts
@@ -26,7 +26,9 @@ import { CoreConstants } from '@/core/constants';
 import { makeSingleton, Platform, Translate } from '@singletons';
 import { CoreStatusWithWarningsWSResponse, CoreWSExternalFile, CoreWSExternalWarning } from '@services/ws';
 
-import { CoreCourseStatusDBRecord, COURSE_STATUS_TABLE } from './database/course';
+import {
+    CoreCourseStatusDBRecord, CoreCourseViewedModulesDBRecord, COURSE_STATUS_TABLE, COURSE_VIEWED_MODULES_TABLE ,
+} from './database/course';
 import { CoreCourseOffline } from './course-offline';
 import { CoreError } from '@classes/errors/error';
 import {
@@ -49,6 +51,7 @@ import { lazyMap, LazyMap } from '@/core/utils/lazy-map';
 import { asyncInstance, AsyncInstance } from '@/core/utils/async-instance';
 import { CoreDatabaseTable } from '@classes/database/database-table';
 import { CoreDatabaseCachingStrategy } from '@classes/database/database-table-proxy';
+import { SQLiteDB } from '@classes/sqlitedb';
 
 const ROOT_CACHE_KEY = 'mmCourse:';
 
@@ -140,6 +143,7 @@ export class CoreCourseProvider {
 
     protected logger: CoreLogger;
     protected statusTables: LazyMap<AsyncInstance<CoreDatabaseTable<CoreCourseStatusDBRecord>>>;
+    protected viewedModulesTables: LazyMap<AsyncInstance<CoreDatabaseTable<CoreCourseViewedModulesDBRecord, 'courseId' | 'cmId'>>>;
 
     constructor() {
         this.logger = CoreLogger.getInstance('CoreCourseProvider');
@@ -149,6 +153,17 @@ export class CoreCourseProvider {
                     siteId,
                     config: { cachingStrategy: CoreDatabaseCachingStrategy.Eager },
                     onDestroy: () => delete this.statusTables[siteId],
+                }),
+            ),
+        );
+
+        this.viewedModulesTables = lazyMap(
+            siteId => asyncInstance(
+                () => CoreSites.getSiteTable<CoreCourseViewedModulesDBRecord, 'courseId' | 'cmId'>(COURSE_VIEWED_MODULES_TABLE, {
+                    siteId,
+                    config: { cachingStrategy: CoreDatabaseCachingStrategy.None },
+                    primaryKeyColumns: ['courseId', 'cmId'],
+                    onDestroy: () => delete this.viewedModulesTables[siteId],
                 }),
             ),
         );
@@ -341,6 +356,31 @@ export class CoreCourseProvider {
     }
 
     /**
+     * Get certain module viewed records in the app.
+     *
+     * @param ids Module IDs.
+     * @param siteId Site ID. If not defined, current site.
+     * @return Promise resolved with map of last module viewed data.
+     */
+    async getCertainModulesViewed(ids: number[] = [], siteId?: string): Promise<Record<number, CoreCourseViewedModulesDBRecord>> {
+        if (!ids.length) {
+            return {};
+        }
+
+        const site = await CoreSites.getSite(siteId);
+
+        const whereAndParams = SQLiteDB.getInOrEqual(ids);
+
+        const entries = await this.viewedModulesTables[site.getId()].getManyWhere({
+            sql: 'cmId ' + whereAndParams.sql,
+            sqlParams: whereAndParams.params,
+            js: (record) => ids.includes(record.cmId),
+        });
+
+        return CoreUtils.arrayToObject(entries, 'cmId');
+    }
+
+    /**
      * Get course blocks.
      *
      * @param courseId Course ID.
@@ -423,6 +463,19 @@ export class CoreCourseProvider {
         });
 
         return entries.map((entry) => entry.id);
+    }
+
+    /**
+     * Get last module viewed in the app for a course.
+     *
+     * @param id Course ID.
+     * @param siteId Site ID. If not defined, current site.
+     * @return Promise resolved with last module viewed data, undefined if none.
+     */
+    async getLastModuleViewed(id: number, siteId?: string): Promise<CoreCourseViewedModulesDBRecord | undefined> {
+        const viewedModules = await this.getViewedModules(id, siteId);
+
+        return viewedModules[0];
     }
 
     /**
@@ -548,7 +601,7 @@ export class CoreCourseProvider {
 
         let foundModule: CoreCourseGetContentsWSModule | undefined;
 
-        const foundSection = sections.some((section) => {
+        const foundSection = sections.find((section) => {
             if (section.id != CoreCourseProvider.STEALTH_MODULES_SECTION_ID &&
                 sectionId !== undefined &&
                 sectionId != section.id
@@ -562,7 +615,7 @@ export class CoreCourseProvider {
         });
 
         if (foundSection && foundModule) {
-            return this.addAdditionalModuleData(foundModule, courseId);
+            return this.addAdditionalModuleData(foundModule, courseId, foundSection.id);
         }
 
         throw new CoreError(Translate.instant('core.course.modulenotfound'));
@@ -573,11 +626,13 @@ export class CoreCourseProvider {
      *
      * @param module Module.
      * @param courseId Course ID of the module.
+     * @param sectionId Section ID of the module.
      * @return Module with additional info.
      */
-    protected  addAdditionalModuleData(
+    protected addAdditionalModuleData(
         module: CoreCourseGetContentsWSModule,
         courseId: number,
+        sectionId: number,
     ): CoreCourseModuleData {
         let completionData: CoreCourseModuleCompletionData | undefined = undefined;
 
@@ -590,13 +645,12 @@ export class CoreCourseProvider {
             };
         }
 
-        const moduleWithCourse: CoreCourseModuleData = {
+        return  {
             ...module,
             course: courseId,
+            section: sectionId,
             completiondata: completionData,
         };
-
-        return moduleWithCourse;
     }
 
     /**
@@ -873,7 +927,7 @@ export class CoreCourseProvider {
         // Add course to all modules.
         return sections.map((section) => ({
             ...section,
-            modules: section.modules.map((module) => this.addAdditionalModuleData(module, courseId)),
+            modules: section.modules.map((module) => this.addAdditionalModuleData(module, courseId, section.id)),
         }));
     }
 
@@ -899,6 +953,23 @@ export class CoreCourseProvider {
         }
 
         return sections.reduce((previous: CoreCourseModuleData[], section) => previous.concat(section.modules || []), []);
+    }
+
+    /**
+     * Get all viewed modules in a course, ordered by timeaccess in descending order.
+     *
+     * @param courseId Course ID.
+     * @param siteId Site ID. If not defined, current site.
+     * @return Promise resolved with the list of viewed modules.
+     */
+    async getViewedModules(courseId: number, siteId?: string): Promise<CoreCourseViewedModulesDBRecord[]> {
+        const site = await CoreSites.getSite(siteId);
+
+        return await this.viewedModulesTables[site.getId()].getMany({ courseId }, {
+            sorting: [
+                { timeaccess: 'desc' },
+            ],
+        });
     }
 
     /**
@@ -1349,6 +1420,34 @@ export class CoreCourseProvider {
     }
 
     /**
+     * Store activity as viewed.
+     *
+     * @param courseId Chapter ID.
+     * @param cmId Module ID.
+     * @param options Other options.
+     * @return Promise resolved with last chapter viewed, undefined if none.
+     */
+    async storeModuleViewed(courseId: number, cmId: number, options: CoreCourseStoreModuleViewedOptions = {}): Promise<void> {
+        const site = await CoreSites.getSite(options.siteId);
+
+        const timeaccess = options.timeaccess ?? Date.now();
+
+        await this.viewedModulesTables[site.getId()].insert({
+            courseId,
+            cmId,
+            sectionId: options.sectionId,
+            timeaccess,
+        });
+
+        CoreEvents.trigger(CoreEvents.COURSE_MODULE_VIEWED, {
+            courseId,
+            cmId,
+            timeaccess,
+            sectionId: options.sectionId,
+        }, site.getId());
+    }
+
+    /**
      * Translate a module name to current language.
      *
      * @param moduleName The module name.
@@ -1769,4 +1868,13 @@ type CoreCompletionUpdateActivityCompletionStatusManuallyWSParams = {
  */
 export type CoreCourseAnyModuleData = CoreCourseModuleData | CoreCourseModuleBasicInfo & {
     contents?: CoreCourseModuleContentFile[]; // If needed, calculated in the app in loadModuleContents.
+};
+
+/**
+ * Options for storeModuleViewed.
+ */
+export type CoreCourseStoreModuleViewedOptions = {
+    sectionId?: number;
+    timeaccess?: number;
+    siteId?: string;
 };

src/addons/block/recentlyaccesseditems/services/recentlyaccesseditems.ts
@@ -49,17 +49,41 @@ export class AddonBlockRecentlyAccessedItemsProvider {
             cacheKey: this.getRecentItemsCacheKey(),
         };
 
-        const items: AddonBlockRecentlyAccessedItemsItem[] =
+        let items: AddonBlockRecentlyAccessedItemsItem[] =
             await site.read('block_recentlyaccesseditems_get_recent_items', undefined, preSets);
 
-        return await Promise.all(items.map(async (item) => {
+        const cmIds: number[] = [];
+
+        items = await Promise.all(items.map(async (item) => {
             const modicon = item.icon && CoreDomUtils.getHTMLElementAttribute(item.icon, 'src');
 
             item.iconUrl = await CoreCourse.getModuleIconSrc(item.modname, modicon || undefined);
             item.iconTitle = item.icon && CoreDomUtils.getHTMLElementAttribute(item.icon, 'title');
+            cmIds.push(item.cmid);
 
             return item;
         }));
+
+        // Check if the viewed module should be updated for each activity.
+        const lastViewedMap = await CoreCourse.getCertainModulesViewed(cmIds, site.getId());
+
+        items.forEach((recentItem) => {
+            const timeAccess = recentItem.timeaccess * 1000;
+            const lastViewed = lastViewedMap[recentItem.cmid];
+
+            if (lastViewed && lastViewed.timeaccess >= timeAccess) {
+                return; // No need to update.
+            }
+
+            // Update access.
+            CoreCourse.storeModuleViewed(recentItem.courseid, recentItem.cmid, {
+                timeaccess: recentItem.timeaccess * 1000,
+                sectionId: lastViewed && lastViewed.sectionId,
+                siteId: site.getId(),
+            });
+        });
+
+        return items;
     }
 
     /**

src/addons/mod/book/services/book.ts
@@ -391,7 +391,7 @@ export class AddonModBookProvider {
     async storeLastChapterViewed(id: number, chapterId: number, courseId: number, siteId?: string): Promise<void> {
         const site = await CoreSites.getSite(siteId);
 
-        await site.storeLastViewed(AddonModBookProvider.COMPONENT, id, chapterId, String(courseId));
+        await site.storeLastViewed(AddonModBookProvider.COMPONENT, id, chapterId, { data: String(courseId) });
     }
 
 }

src/addons/mod/data/pages/entry/entry.ts
@@ -204,7 +204,10 @@ export class AddonModDataEntryPage implements OnInit, OnDestroy {
 
             if (this.logAfterFetch) {
                 this.logAfterFetch = false;
-                this.logView();
+                await CoreUtils.ignoreErrors(AddonModData.logView(this.database.id, this.database.name));
+
+                // Store module viewed. It's done in this page because it can be reached using a link.
+                CoreCourse.storeModuleViewed(this.courseId, this.moduleId);
             }
         } catch (error) {
             if (!refresh) {
@@ -400,19 +403,6 @@ export class AddonModDataEntryPage implements OnInit, OnDestroy {
      */
     ratingUpdated(): void {
         AddonModData.invalidateEntryData(this.database!.id, this.entryId!);
-    }
-
-    /**
-     * Log viewing the activity.
-     *
-     * @return Promise resolved when done.
-     */
-    protected async logView(): Promise<void> {
-        if (!this.database || !this.database.id) {
-            return;
-        }
-
-        await CoreUtils.ignoreErrors(AddonModData.logView(this.database.id, this.database.name));
     }
 
     /**

src/addons/mod/feedback/pages/attempt/attempt.ts
@@ -16,6 +16,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRouteSnapshot } from '@angular/router';
 import { CoreRoutedItemsManagerSourcesTracker } from '@classes/items-management/routed-items-manager-sources-tracker';
 import { CoreSwipeNavigationItemsManager } from '@classes/items-management/swipe-navigation-items-manager';
+import { CoreCourse } from '@features/course/services/course';
 import { CoreNavigator } from '@services/navigator';
 import { CoreDomUtils } from '@services/utils/dom';
 import { CoreTextUtils } from '@services/utils/text';
@@ -49,6 +50,7 @@ export class AddonModFeedbackAttemptPage implements OnInit, OnDestroy {
     loaded = false;
 
     protected attemptId: number;
+    protected fetchSuccess = false;
 
     constructor() {
         this.cmId = CoreNavigator.getRequiredRouteNumberParam('cmId');
@@ -131,6 +133,11 @@ export class AddonModFeedbackAttemptPage implements OnInit, OnDestroy {
                 return attemptItem;
             }).filter((itemData) => itemData); // Filter items with errors.
 
+            if (!this.fetchSuccess) {
+                this.fetchSuccess = true;
+                // Store module viewed. It's done in this page because it can be reached using a link.
+                CoreCourse.storeModuleViewed(this.courseId, this.cmId);
+            }
         } catch (message) {
             // Some call failed on fetch, go back.
             CoreDomUtils.showErrorModalDefault(message, 'core.course.errorgetmodule', true);

src/addons/mod/feedback/pages/attempts/attempts.ts
@@ -25,6 +25,7 @@ import { CoreDomUtils } from '@services/utils/dom';
 import { CoreUtils } from '@services/utils/utils';
 import { AddonModFeedbackAttemptItem, AddonModFeedbackAttemptsSource } from '../../classes/feedback-attempts-source';
 import { AddonModFeedbackWSAnonAttempt, AddonModFeedbackWSAttempt } from '../../services/feedback';
+import { CoreCourse } from '@features/course/services/course';
 
 /**
  * Page that displays feedback attempts.
@@ -37,14 +38,14 @@ export class AddonModFeedbackAttemptsPage implements AfterViewInit, OnDestroy {
 
     @ViewChild(CoreSplitViewComponent) splitView!: CoreSplitViewComponent;
 
-    promisedAttempts: CorePromisedValue<CoreListItemsManager<AddonModFeedbackAttemptItem, AddonModFeedbackAttemptsSource>>;
+    promisedAttempts: CorePromisedValue<AddonModFeedbackAttemptsManager>;
     fetchFailed = false;
 
     constructor(protected route: ActivatedRoute) {
         this.promisedAttempts = new CorePromisedValue();
     }
 
-    get attempts(): CoreListItemsManager<AddonModFeedbackAttemptItem, AddonModFeedbackAttemptsSource> | null {
+    get attempts(): AddonModFeedbackAttemptsManager | null {
         return this.promisedAttempts.value;
     }
 
@@ -95,7 +96,7 @@ export class AddonModFeedbackAttemptsPage implements AfterViewInit, OnDestroy {
 
             source.selectedGroup = CoreNavigator.getRouteNumberParam('group') || 0;
 
-            this.promisedAttempts.resolve(new CoreListItemsManager(source, this.route.component));
+            this.promisedAttempts.resolve(new AddonModFeedbackAttemptsManager(source, this.route.component));
         } catch (error) {
             CoreDomUtils.showErrorModal(error);
 
@@ -178,6 +179,21 @@ export class AddonModFeedbackAttemptsPage implements AfterViewInit, OnDestroy {
         const attempts = await this.promisedAttempts;
 
         await attempts.reload();
+    }
+
+}
+
+/**
+ * Attempts manager.
+ */
+class AddonModFeedbackAttemptsManager extends CoreListItemsManager<AddonModFeedbackAttemptItem, AddonModFeedbackAttemptsSource> {
+
+    /**
+     * @inheritdoc
+     */
+    protected async logActivity(): Promise<void> {
+        // Store module viewed. It's done in this page because it can be reached using a link.
+        CoreCourse.storeModuleViewed(this.getSource().COURSE_ID, this.getSource().CM_ID);
     }
 
 }

src/addons/mod/feedback/pages/nonrespondents/nonrespondents.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import { Component, OnInit } from '@angular/core';
+import { CoreCourse } from '@features/course/services/course';
 import { IonRefresher } from '@ionic/angular';
 import { CoreGroupInfo, CoreGroups } from '@services/groups';
 import { CoreNavigator } from '@services/navigator';
@@ -34,6 +35,7 @@ export class AddonModFeedbackNonRespondentsPage implements OnInit {
     protected courseId!: number;
     protected feedback?: AddonModFeedbackWSFeedback;
     protected page = 0;
+    protected fetchSuccess = false;
 
     selectedGroup!: number;
     groupInfo?: CoreGroupInfo;
@@ -81,6 +83,12 @@ export class AddonModFeedbackNonRespondentsPage implements OnInit {
             this.selectedGroup = CoreGroups.validateGroupId(this.selectedGroup, this.groupInfo);
 
             await this.loadGroupUsers(this.selectedGroup);
+
+            if (!this.fetchSuccess) {
+                this.fetchSuccess = true;
+                // Store module viewed. It's done in this page because it can be reached using a link.
+                CoreCourse.storeModuleViewed(this.courseId, this.cmId);
+            }
         } catch (message) {
             CoreDomUtils.showErrorModalDefault(message, 'core.course.errorgetmodule', true);
 

jerico-castillo/my-moodleapp

commit 100ababf616103987f956fc10d987d456ada85fb

MOBILE-3833 course: Fix manual completion db
Issue introduced by mistake in https://github.com/moodlehq/moodleapp/commit/b72e247f81f2bda9bafd689af12ec493c42c1c43#diff-d5618a894713624722f8d892b9befe9b865f16cc00593ee23f420047917d3fcaR112

src/core/features/course/services/database/course.ts
@@ -109,7 +109,7 @@ export const OFFLINE_SITE_SCHEMA: CoreSiteSchema = {
                     type: 'TEXT',
                 },
                 {
-                    name: 'timeaccess',
+                    name: 'timecompleted',
                     type: 'INTEGER',
                 },
             ],