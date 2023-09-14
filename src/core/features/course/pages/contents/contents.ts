NoelDeMartin/moodleapp

MOBILE-3371 search: Support blocks and sections

src/core/features/course/pages/contents/contents.ts
@@ -58,6 +58,7 @@ export class CoreCourseContentsPage implements OnInit, OnDestroy, CoreRefreshCon
     sections?: CoreCourseSection[];
     sectionId?: number;
     sectionNumber?: number;
+    instanceId?: number;
     dataLoaded = false;
     updatingData = false;
     downloadCourseEnabled = false;
@@ -92,6 +93,7 @@ export class CoreCourseContentsPage implements OnInit, OnDestroy, CoreRefreshCon
 
         this.sectionId = CoreNavigator.getRouteNumberParam('sectionId');
         this.sectionNumber = CoreNavigator.getRouteNumberParam('sectionNumber');
+        this.instanceId = CoreNavigator.getRouteNumberParam('instanceId');
         this.moduleId = CoreNavigator.getRouteNumberParam('moduleId');
         this.isGuest = CoreNavigator.getRouteBooleanParam('isGuest');
 

src/core/features/course/pages/index/index.ts
@@ -74,7 +74,7 @@ export class CoreCourseIndexPage implements OnInit, OnDestroy {
                 if (data.sectionId) {
                     this.contentsTab.pageParams.sectionId = data.sectionId;
                 }
-                if (data.sectionNumber) {
+                if (data.sectionNumber !== undefined) {
                     this.contentsTab.pageParams.sectionNumber = data.sectionNumber;
                 }
 
@@ -162,12 +162,13 @@ export class CoreCourseIndexPage implements OnInit, OnDestroy {
             course: this.course,
             sectionId: CoreNavigator.getRouteNumberParam('sectionId'),
             sectionNumber: CoreNavigator.getRouteNumberParam('sectionNumber'),
+            instanceId: CoreNavigator.getRouteNumberParam('instanceId'),
             isGuest: this.isGuest,
         };
 
         if (this.module) {
             this.contentsTab.pageParams.moduleId = this.module.id;
-            if (!this.contentsTab.pageParams.sectionId && !this.contentsTab.pageParams.sectionNumber) {
+            if (!this.contentsTab.pageParams.sectionId && this.contentsTab.pageParams.sectionNumber === undefined) {
                 // No section specified, use module section.
                 this.contentsTab.pageParams.sectionId = this.module.section;
             }

src/core/features/courses/services/handlers/course-link.ts
@@ -69,6 +69,12 @@ export class CoreCoursesCourseLinkHandlerService extends CoreContentLinksHandler
 
         if (!isNaN(sectionNumber)) {
             pageParams.sectionNumber = sectionNumber;
+        } else {
+            const matches = url.match(/#inst(\d+)/);
+
+            if (matches && matches[1]) {
+                pageParams.instanceId = parseInt(matches[1], 10);
+            }
         }
 
         return [{
@@ -136,7 +142,7 @@ export class CoreCoursesCourseLinkHandlerService extends CoreContentLinksHandler
             // Direct access.
             const course = await CoreUtils.ignoreErrors(CoreCourses.getUserCourse(courseId), { id: courseId });
 
-            CoreCourseHelper.openCourse(course, pageParams);
+            CoreCourseHelper.openCourse(course, { params: pageParams });
         } else {
             this.navigateCourseSummary(courseId, pageParams);
 

src/core/features/sitehome/pages/index/index.ts
@@ -14,7 +14,7 @@
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IonRefresher } from '@ionic/angular';
-import { Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { CoreSite, CoreSiteConfig } from '@classes/site';
 import { CoreCourse, CoreCourseWSSection } from '@features/course/services/course';
@@ -31,6 +31,7 @@ import { CoreBlockHelper } from '@features/block/services/block-helper';
 import { CoreUtils } from '@services/utils/utils';
 import { CoreTime } from '@singletons/time';
 import { CoreAnalytics, CoreAnalyticsEventType } from '@services/analytics';
+import { CoreBlockSideBlocksComponent } from '@features/block/components/side-blocks/side-blocks';
 
 /**
  * Page that displays site home index.
@@ -58,7 +59,7 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
     protected updateSiteObserver: CoreEventObserver;
     protected logView: () => void;
 
-    constructor() {
+    constructor(protected route: ActivatedRoute) {
         // Refresh the enabled flags if site is updated.
         this.updateSiteObserver = CoreEvents.on(CoreEvents.SITE_UPDATED, () => {
             this.searchEnabled = !CoreCourses.isSearchCoursesDisabledInSite();
@@ -102,6 +103,10 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
         this.loadContent().finally(() => {
             this.dataLoaded = true;
         });
+
+        this.openFocusedInstance();
+
+        this.route.queryParams.subscribe(() => this.openFocusedInstance());
     }
 
     /**
@@ -224,6 +229,24 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
      */
     ngOnDestroy(): void {
         this.updateSiteObserver.off();
+    }
+
+    /**
+     * Check whether there is a focused instance in the page parameters and open it.
+     */
+    private openFocusedInstance() {
+        const focusInstanceId = CoreNavigator.getRouteNumberParam('focusInstanceId');
+
+        if (focusInstanceId) {
+            CoreDomUtils.openSideModal({
+                component: CoreBlockSideBlocksComponent,
+                componentProps: {
+                    contextLevel: 'course',
+                    instanceId: this.siteHomeId,
+                    initialBlockInstanceId: focusInstanceId,
+                },
+            });
+        }
     }
 
 }

src/core/features/sitehome/services/handlers/index-link.ts
@@ -22,6 +22,7 @@ import { makeSingleton } from '@singletons';
 import { CoreNavigator } from '@services/navigator';
 import { CoreSiteHomeHomeHandlerService } from './sitehome-home';
 import { CoreMainMenuHomeHandlerService } from '@features/mainmenu/services/handlers/mainmenu';
+import { Params } from '@angular/router';
 
 /**
  * Handler to treat links to site home index.
@@ -36,7 +37,14 @@ export class CoreSiteHomeIndexLinkHandlerService extends CoreContentLinksHandler
     /**
      * @inheritdoc
      */
-    getActions(): CoreContentLinksAction[] | Promise<CoreContentLinksAction[]> {
+    getActions(siteIds: string[], url: string): CoreContentLinksAction[] | Promise<CoreContentLinksAction[]> {
+        const pageParams: Params = {};
+        const matches = url.match(/#inst(\d+)/);
+
+        if (matches && matches[1]) {
+            pageParams.focusInstanceId = parseInt(matches[1], 10);
+        }
+
         return [{
             action: (siteId: string): void => {
                 CoreNavigator.navigateToSitePath(
@@ -44,6 +52,7 @@ export class CoreSiteHomeIndexLinkHandlerService extends CoreContentLinksHandler
                     {
                         preferCurrentTab: false,
                         siteId,
+                        params: pageParams,
                     },
                 );
             },

jerico-castillo/my-moodleapp

src/core/features/course/pages/contents/contents.ts
@@ -58,6 +58,7 @@ export class CoreCourseContentsPage implements OnInit, OnDestroy, CoreRefreshCon
     sections?: CoreCourseSection[];
     sectionId?: number;
     sectionNumber?: number;
+    blockInstanceId?: number;
     dataLoaded = false;
     updatingData = false;
     downloadCourseEnabled = false;
@@ -92,6 +93,7 @@ export class CoreCourseContentsPage implements OnInit, OnDestroy, CoreRefreshCon
 
         this.sectionId = CoreNavigator.getRouteNumberParam('sectionId');
         this.sectionNumber = CoreNavigator.getRouteNumberParam('sectionNumber');
+        this.blockInstanceId = CoreNavigator.getRouteNumberParam('blockInstanceId');
         this.moduleId = CoreNavigator.getRouteNumberParam('moduleId');
         this.isGuest = CoreNavigator.getRouteBooleanParam('isGuest');
 

src/core/features/course/pages/index/index.ts
@@ -74,7 +74,7 @@ export class CoreCourseIndexPage implements OnInit, OnDestroy {
                 if (data.sectionId) {
                     this.contentsTab.pageParams.sectionId = data.sectionId;
                 }
-                if (data.sectionNumber) {
+                if (data.sectionNumber !== undefined) {
                     this.contentsTab.pageParams.sectionNumber = data.sectionNumber;
                 }
 
@@ -162,12 +162,13 @@ export class CoreCourseIndexPage implements OnInit, OnDestroy {
             course: this.course,
             sectionId: CoreNavigator.getRouteNumberParam('sectionId'),
             sectionNumber: CoreNavigator.getRouteNumberParam('sectionNumber'),
+            blockInstanceId: CoreNavigator.getRouteNumberParam('blockInstanceId'),
             isGuest: this.isGuest,
         };
 
         if (this.module) {
             this.contentsTab.pageParams.moduleId = this.module.id;
-            if (!this.contentsTab.pageParams.sectionId && !this.contentsTab.pageParams.sectionNumber) {
+            if (!this.contentsTab.pageParams.sectionId && this.contentsTab.pageParams.sectionNumber === undefined) {
                 // No section specified, use module section.
                 this.contentsTab.pageParams.sectionId = this.module.section;
             }

src/core/features/courses/services/handlers/course-link.ts
@@ -69,6 +69,12 @@ export class CoreCoursesCourseLinkHandlerService extends CoreContentLinksHandler
 
         if (!isNaN(sectionNumber)) {
             pageParams.sectionNumber = sectionNumber;
+        } else {
+            const matches = url.match(/#inst(\d+)/);
+
+            if (matches && matches[1]) {
+                pageParams.blockInstanceId = parseInt(matches[1], 10);
+            }
         }
 
         return [{
@@ -136,7 +142,7 @@ export class CoreCoursesCourseLinkHandlerService extends CoreContentLinksHandler
             // Direct access.
             const course = await CoreUtils.ignoreErrors(CoreCourses.getUserCourse(courseId), { id: courseId });
 
-            CoreCourseHelper.openCourse(course, pageParams);
+            CoreCourseHelper.openCourse(course, { params: pageParams });
         } else {
             this.navigateCourseSummary(courseId, pageParams);
 

src/core/features/sitehome/pages/index/index.ts
@@ -14,7 +14,7 @@
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IonRefresher } from '@ionic/angular';
-import { Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { CoreSite, CoreSiteConfig } from '@classes/site';
 import { CoreCourse, CoreCourseWSSection } from '@features/course/services/course';
@@ -31,6 +31,7 @@ import { CoreBlockHelper } from '@features/block/services/block-helper';
 import { CoreUtils } from '@services/utils/utils';
 import { CoreTime } from '@singletons/time';
 import { CoreAnalytics, CoreAnalyticsEventType } from '@services/analytics';
+import { CoreBlockSideBlocksComponent } from '@features/block/components/side-blocks/side-blocks';
 
 /**
  * Page that displays site home index.
@@ -58,7 +59,7 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
     protected updateSiteObserver: CoreEventObserver;
     protected logView: () => void;
 
-    constructor() {
+    constructor(protected route: ActivatedRoute) {
         // Refresh the enabled flags if site is updated.
         this.updateSiteObserver = CoreEvents.on(CoreEvents.SITE_UPDATED, () => {
             this.searchEnabled = !CoreCourses.isSearchCoursesDisabledInSite();
@@ -102,6 +103,10 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
         this.loadContent().finally(() => {
             this.dataLoaded = true;
         });
+
+        this.openFocusedInstance();
+
+        this.route.queryParams.subscribe(() => this.openFocusedInstance());
     }
 
     /**
@@ -224,6 +229,24 @@ export class CoreSiteHomeIndexPage implements OnInit, OnDestroy {
      */
     ngOnDestroy(): void {
         this.updateSiteObserver.off();
+    }
+
+    /**
+     * Check whether there is a focused instance in the page parameters and open it.
+     */
+    private openFocusedInstance() {
+        const blockInstanceId = CoreNavigator.getRouteNumberParam('blockInstanceId');
+
+        if (blockInstanceId) {
+            CoreDomUtils.openSideModal({
+                component: CoreBlockSideBlocksComponent,
+                componentProps: {
+                    contextLevel: 'course',
+                    instanceId: this.siteHomeId,
+                    initialBlockInstanceId: blockInstanceId,
+                },
+            });
+        }
     }
 
 }

src/core/features/sitehome/services/handlers/index-link.ts
@@ -22,6 +22,7 @@ import { makeSingleton } from '@singletons';
 import { CoreNavigator } from '@services/navigator';
 import { CoreSiteHomeHomeHandlerService } from './sitehome-home';
 import { CoreMainMenuHomeHandlerService } from '@features/mainmenu/services/handlers/mainmenu';
+import { Params } from '@angular/router';
 
 /**
  * Handler to treat links to site home index.
@@ -36,7 +37,14 @@ export class CoreSiteHomeIndexLinkHandlerService extends CoreContentLinksHandler
     /**
      * @inheritdoc
      */
-    getActions(): CoreContentLinksAction[] | Promise<CoreContentLinksAction[]> {
+    getActions(siteIds: string[], url: string): CoreContentLinksAction[] | Promise<CoreContentLinksAction[]> {
+        const pageParams: Params = {};
+        const matches = url.match(/#inst(\d+)/);
+
+        if (matches && matches[1]) {
+            pageParams.blockInstanceId = parseInt(matches[1], 10);
+        }
+
         return [{
             action: (siteId: string): void => {
                 CoreNavigator.navigateToSitePath(
@@ -44,6 +52,7 @@ export class CoreSiteHomeIndexLinkHandlerService extends CoreContentLinksHandler
                     {
                         preferCurrentTab: false,
                         siteId,
+                        params: pageParams,
                     },
                 );
             },